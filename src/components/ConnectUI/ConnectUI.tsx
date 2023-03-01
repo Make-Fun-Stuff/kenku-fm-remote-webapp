af2ca19374819eb9dfd8e4544bb302f6bb37269e
Gavin Scott <gavin.o.scott@gmail.com>
2023-02-28 16:14:13 -0800

    Move soundboards into table
    Moved soundboards into a table, with two buttons in each row. Both
    will play all sounds in the soundboard, but one will stop all sounds
    that are currently playing first.

--- src/components/ConnectUI/ConnectUI.tsx
+++ src/components/ConnectUI/ConnectUI.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, TextField, Typography } from "@mui/material";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { listPlaylists } from "../../kenku/playlist";
 
@@ -25,6 +25,10 @@ function ConnectUI(props: ConnectUIProps) {
       setShowError(true);
     }
   }, [connectionSuccess, host, port]);
+
+  useEffect(() => {
+    testConnection().catch(console.error);
+  });
 
   return (
     <Card
--- src/components/Soundboards/Soundboards.tsx
+++ src/components/Soundboards/Soundboards.tsx
@@ -1,4 +1,15 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { styled } from "@mui/material/styles";
 import { useEffect, useMemo, useState } from "react";
 import {
   play,
@@ -6,23 +17,25 @@ import {
   getPlayback,
   listSoundboards,
   ListSoundboardsResponse,
+  Soundboard,
 } from "../../kenku/soundboard";
 import { sortBy } from "lodash";
-import SoundboardButton from "../SoundboardButton/SoundboardButton";
 import { useCookies } from "react-cookie";
 import { KenkuRemoteConfig } from "../../kenku/kenku";
-
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
 
 export interface SoundboardsProps {
   connectionFailure: () => void;
 }
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 function Soundboards(props: SoundboardsProps) {
   const [loading, setLoading] = useState(true);
@@ -31,9 +44,6 @@ function Soundboards(props: SoundboardsProps) {
     soundboards: [],
     sounds: [],
   });
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cookies, _setCookie] = useCookies(["host", "port"]);
   const kenkuConfig: KenkuRemoteConfig = useMemo(
@@ -61,29 +71,17 @@ function Soundboards(props: SoundboardsProps) {
     return () => clearInterval(interval);
   }, [kenkuConfig, connectionFailure]);
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   if (loading) {
     return <div />;
   }
 
-  let itemWidth = 2;
-  if (windowDimensions.width < 500) {
-    itemWidth = 12;
-  } else if (windowDimensions.width < 750) {
-    itemWidth = 6;
-  } else if (windowDimensions.width < 1000) {
-    itemWidth = 4;
-  } else if (windowDimensions.width < 1700) {
-    itemWidth = 3;
-  }
+  const playSoundEffects = async (soundboard: Soundboard, replace: boolean) => {
+    if (replace) {
+      const playback = await getPlayback(kenkuConfig);
+      await Promise.all(playback.sounds.map((_) => stop(kenkuConfig, _.id)));
+    }
+    await Promise.all(soundboard.sounds.map((_) => play(kenkuConfig, _)));
+  };
 
   return hadError ? (
     <div />
@@ -92,34 +90,61 @@ function Soundboards(props: SoundboardsProps) {
       style={{
         marginTop: "50px",
         marginBottom: "50px",
+        marginLeft: "30px",
+        marginRight: "30px",
       }}
     >
       <Typography variant="h5" marginBottom={"20px"}>
         Sound Effects
       </Typography>
-      <Grid container spacing={2}>
-        {sortBy(
-          soundboards.soundboards,
-          (_) => `${_.sounds.length ? "" : "zzz"}${_.title}`
-        ).map((soundboard, index) => {
-          return (
-            <Grid item xs={itemWidth} key={index}>
-              <SoundboardButton
-                soundboard={soundboard}
-                play={async () => {
-                  const playback = await getPlayback(kenkuConfig);
-                  await Promise.all(
-                    playback.sounds.map((_) => stop(kenkuConfig, _.id))
-                  );
-                  await Promise.all(
-                    soundboard.sounds.map((_) => play(kenkuConfig, _))
-                  );
-                }}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Sound Effect Group</StyledTableCell>
+              <StyledTableCell align="center">
+                Add to Current Sound Effects
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                Replace Current Sound Effects
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortBy(
+              soundboards.soundboards,
+              (_) => `${_.sounds.length ? "" : "zzz"}${_.title}`
+            ).map((soundboard, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <StyledTableCell component="th" scope="row">
+                  {soundboard.title.toUpperCase()}
+                </StyledTableCell>
+                <StyledTableCell component="th" scope="row" align="center">
+                  <Button
+                    variant="contained"
+                    disabled={!soundboard.sounds.length}
+                    onClick={() => playSoundEffects(soundboard, false)}
+                  >
+                    Add
+                  </Button>
+                </StyledTableCell>
+                <StyledTableCell component="th" scope="row" align="center">
+                  <Button
+                    variant="contained"
+                    disabled={!soundboard.sounds.length}
+                    onClick={() => playSoundEffects(soundboard, true)}
+                  >
+                    Replace
+                  </Button>
+                </StyledTableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
